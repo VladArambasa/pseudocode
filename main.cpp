5f64654ca342a3d8e30bb83c46760a22c3f78086
Vlad Arambașa <vlad.arambasa@gmail.com>
2025-05-23 08:28:51 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include "algoritmica.h"
+
+
+Inceput
+	nr_natural n;
+	citeste n;
+	daca (n % 12 == 0) atunci
+		scrie "DIVIZIBIL";
+		scrie rand_nou;
+	altfel
+		scrie "NEDIVIZIBIL";
+		scrie rand_nou;
+	sfarsit_daca
+	de_la_inceput;
+	
+	pentru(i,0,-10,-2) executa
+		scrie i;
+		scrie rand_nou;
+	sfarsit_pentru
+	
+Sfarsit
